97eaa0dca9b42157d54c03191010aaff576eb00a
Ani Maghradze <animaghradze50@gmail.com>
2024-10-04 00:26:41 +0400

    Create NotificationBar.tsx

---
+++ src/components/NotificationBar.tsx
@@ -0,0 +1,9 @@
+export function NotificationBar(){
+    return (
+        <div className="bg-[#0E1422] h-10 flex justify-center items-center">
+            <div className="container mx-auto">
+                <h1 className="text-white text-center">Get 25% OFF on your first order. Order Now</h1>
+            </div>
+        </div>
+    )
+}
