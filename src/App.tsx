a5329a29512848c270e764d31efe070f55eee257
Ani Maghradze <animaghradze50@gmail.com>
2024-10-04 00:27:54 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import {Homepage} from "./pages/Homepage";
 
-function App() {
+function App(){
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/" element={<Homepage/>}/>
             </Routes>
         </div>
     );
 }
 
-const Home = () => <h2>Home Page</h2>;
-const About = () => <h2>About Page</h2>;
-const Contact = () => <h2>Contact Page</h2>;
 
 export default App;
 
-//redux
