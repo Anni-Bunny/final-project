5ae85e1b00bcd2379eba8cf9a4eba7dc529d04b3
Ani Maghradze <animaghradze50@gmail.com>
2024-10-04 00:27:21 +0400

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import {Link} from "react-router-dom";
+import {Icon} from "./Icon";
+import {Container} from "./Container";
+
+export function Header() {
+    return (
+        <div className="h-20 bg-white flex justify-center items-center">
+            <Container className="px-3 h-11 w-3/5 justify-between">
+                <div className="flex gap-32 px-3 py-0.5 items-center">
+                    <div className="flex items-center gap-3">
+                        <img src="/images/ecommerceLogo.svg" alt="Ecommerce logo"/>
+                        <h1 className="font-bold">Ecommerce</h1>
+                    </div>
+                    <div className="flex gap-8">
+                        <Link to={""}>Home</Link>
+                        <Link to={""}>Categories</Link>
+                        <Link to={""}>About</Link>
+                        <Link to={""}>Contact</Link>
+                    </div>
+                </div>
+                <div className="flex gap-8 h-11 items-center">
+                    <div className="flex border border-1 rounded-md gap-2 px-4 py-2.5 h-11 items-center w-72">
+                        <Icon name="search"/>
+                        <input type="search" className="h-6 outline-none"/>
+                    </div>
+                    <Icon name="cart" className="size-"/>
+                    <Icon name="user-1"/>
+                </div>
+            </Container>
+        </div>
+    );
+}
