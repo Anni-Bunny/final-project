a3693c7513dedf6548d33cb54ea5afd92f57b524
Ani Maghradze <animaghradze50@gmail.com>
2024-10-05 16:13:26 +0400

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,9 @@
-export function Button() {
-    return (
-        <></>
-    );
+export function Button({children}: any) {
+    return <button
+        className="overflow-hidden group/button relative rounded bg-black border p-2 transition duration-200 flex">
+        <span className="invert">{children}</span>
+        <div
+            className="group-hover/button:top-[-100%] left-0 absolute bg-white w-full h-full top-full transition-all duration-600">
+        </div>
+    </button>;
 }
