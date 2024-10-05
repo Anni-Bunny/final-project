d3f9a8ad6b7e926841c1a4f9373b968d4679b0d5
Ani Maghradze <animaghradze50@gmail.com>
2024-10-05 18:05:59 +0400

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
-export function Button({children}: any) {
+export function Button({children, className = null}: any) {
     return <button
-        className="overflow-hidden group/button relative rounded bg-black border py-3 px-6 transition duration-200 flex h-[2.75rem] items-center">
+        className={`overflow-hidden group/button relative rounded bg-black border py-3 px-6 transition duration-200 flex h-[2.75rem] items-center ${className}`}>
         <span className="invert flex items-center">{children}</span>
         <div
             className="group-hover/button:top-[-100%] left-0 absolute bg-white w-full h-full top-full transition-all duration-600">
