e35b4f80400673944abb7551f61e6e937a1c9ec9
Ani Maghradze <animaghradze50@gmail.com>
2024-10-06 02:48:23 +0400

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,15 @@
 import {Icon} from "./Icon";
 interface CardProps {
-    name: string,
+    icon: string,
     heading: string,
     paragraph: string
 }
 
-export function Card({name, heading, paragraph }:CardProps) {
+export function Card({icon, heading, paragraph }:CardProps) {
     return (
         <div
             className="max-w-sm p-6 bg-white flex flex-col gap-3">
-            <Icon name={`${name}`}/>
+            <Icon name={`${icon}`}/>
             <h5 className="font-bold mt-3">{heading}</h5>
             <p className="text-[0.875rem]">{paragraph}</p>
         </div>
