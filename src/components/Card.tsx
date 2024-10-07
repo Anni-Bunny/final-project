cc81cf5ec2044421ef6c18c5859fda461c03406a
Ani Maghradze <animaghradze50@gmail.com>
2024-10-08 00:26:17 +0400

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
-export function Card({children}: any) {
+export function Card({children, className}: any) {
     return (
         <div
-            className="max-w-sm p-6 bg-white flex flex-col gap-3">
+            className={`max-w-sm bg-white flex flex-col gap-3 ${className}`}>
             {children}
         </div>
     );
