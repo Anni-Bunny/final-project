3cfa981b978cb91bbd4f43c0ab07f93d2e2fe082
Ani Maghradze <animaghradze50@gmail.com>
2024-10-10 18:15:54 +0400

    Update Button.tsx
    added type prop and used switch case to create variety of buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,75 @@
 import {Icon} from "./Icon";
-interface ButtonProps{
-    name: string,
-    className?:string,
-    title: string
+
+interface ButtonProps {
+    icon: string,
+    className?: string,
+    title: string,
+    type?: 'button-1' | 'button-2' | 'button-3' | 'button-4' | 'button-5' | 'button-6' | 'button-7' | 'button-8'
 }
 
-export function Button({name, className, title}: ButtonProps) {
+export function Button({icon, className, title, type = "button-1"}: ButtonProps) {
+
+    let typeClasses = ""
+    let divClasses = ""
+    let titleClasses = ""
+
+    switch (type) {
+        case 'button-1': {
+            typeClasses = "bg-[#0E1422] rounded border py-3 px-6 h-[2.75rem]"
+            divClasses = "bg-white"
+            titleClasses = ""
+            break;
+        }
+        case 'button-2': {
+            typeClasses = "bg-[#202533] rounded border py-3 px-6 h-[2.75rem]"
+            divClasses = "bg-white"
+            titleClasses = ""
+            break;
+        }
+        case 'button-3': {
+            typeClasses = "bg-white rounded border py-3 px-6 h-[2.75rem]"
+            divClasses = "bg-gray-500"
+            titleClasses = "text-[#5C5F6A]"
+            break;
+        }
+        case 'button-4': {
+            typeClasses = "bg-white py-3 px-6 h-[2.75rem]"
+            divClasses = "bg-gray-500"
+            titleClasses = "text-[#0E1422]"
+            break;
+        }
+        case 'button-5': {
+            typeClasses = "bg-white rounded border py-3 px-6 h-[2.75rem] border-black"
+            divClasses = "bg-gray-500"
+            titleClasses = "text-[#0E1422]"
+            break;
+        }
+        case 'button-6': {
+            typeClasses = "bg-[#0E1422] rounded border py-3 px-5 h-[2.75rem]"
+            divClasses = "bg-white"
+            titleClasses = ""
+            break;
+        }
+        case 'button-7': {
+            typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem] border-black"
+            divClasses = "bg-gray-500"
+            titleClasses = "text-[#0E1422]"
+            break;
+        }
+        case 'button-8': {
+            typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem]"
+            divClasses = "bg-gray-500"
+            titleClasses = "text-[#5C5F6A]"
+            break;
+        }
+    }
+
     return <button
-        className={`overflow-hidden group/button relative rounded bg-black border py-3 px-6 transition duration-200 flex h-[2.75rem] items-center ${className}`}>
-        <span className="invert flex items-center">{title}</span>
+        className={`${typeClasses} overflow-hidden group/button relative transition duration-200 flex items-center ${className}`}>
+        <span className={`${titleClasses} font-medium invert flex items-center`}>{title}</span>
         <div
-            className="group-hover/button:top-[-100%] left-0 absolute bg-white w-full h-full top-full transition-all duration-600">
+            className={`${divClasses} group-hover/button:top-[-100%] left-0 absolute w-full h-full top-full transition-all duration-600`}>
         </div>
-        <Icon name={`${name}`}/>
+        <Icon name={`${icon}`}/>
     </button>;
 }
