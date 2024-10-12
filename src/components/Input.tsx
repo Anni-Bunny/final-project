9c22f5a86c0b2a2fb3d38076a71c13e887247e62
Ani Maghradze <animaghradze50@gmail.com>
2024-10-12 23:35:30 +0400

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,21 @@
 interface InputProps {
     className?: string,
-    label: string,
-    inputType: string
+    label?: string,
+    inputType: string,
+    placeholder?: string,
+    inputClassName?:string
 }
 
-export function Input({label, className, inputType}: InputProps) {
+export function Input({label, className, inputType, placeholder, inputClassName}: InputProps) {
     return (
         <div className={`flex flex-col ${className}`}>
-            <h3 className="text-sm/6 font-medium text-black">{label}</h3>
-            <input className="rounded-md bg-white border border-[#E6E7E8] py-2.5 px-[0.938rem] h-[2.813rem] w-80 text-black text-sm
-            focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 required:"
-                   type={inputType}/>
+            {
+                (label) ? <label className="text-sm/6 font-medium text-black">{label}</label> : null
+            }
+            <input className={`${inputClassName} rounded-md border border-[#E6E7E8] py-2.5 px-[0.938rem] h-[2.813rem] w-80 text-black text-sm
+            focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 required:`}
+                   type={inputType}
+                   placeholder={placeholder}/>
         </div>
     )
 }
