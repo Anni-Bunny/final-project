e479cb55cb8b30fe571d7401998b2a9e0f53b73d
Ani Maghradze <animaghradze50@gmail.com>
2024-10-15 23:05:30 +0400

    Create Slider.tsx

---
+++ src/components/Slider.tsx
@@ -0,0 +1,26 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import {ProductCard} from "./ProductCard";
+import {productListItem} from "../interfaces/productListItem";
+
+interface ProductSliderInterface {
+    products: productListItem[]
+}
+
+export function Slider({products}: ProductSliderInterface) {
+    return (
+        <Swiper className="flex w-full justify-between"
+                spaceBetween={50}
+                slidesPerView={4}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper: any) => console.log(swiper)}>
+            {
+                products.map(product => (
+                    <SwiperSlide key={product.id}>
+                        <ProductCard key={product.id} product={product}/>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
+    );
+}
