aa7d563d469e7d8ce167e76bd3078d9acfcf0ead
Ani Maghradze <animaghradze50@gmail.com>
2024-10-21 01:48:59 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import {Homepage} from "./pages/Homepage";
+import {Product} from "./pages/Product";
 
 function App(){
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Homepage/>}/>
+                <Route path="/product" element={<Product/>}/>
+
             </Routes>
         </div>
     );
