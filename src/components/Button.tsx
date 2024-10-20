ffb885740fb883b1c81fd1b5f38886234fac361d
Ani Maghradze <animaghradze50@gmail.com>
2024-10-20 23:24:58 +0400

    Update Button.tsx
    created btnTypes object and replaced switch statement with it

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,90 +1,92 @@
 import {Icon} from "./Icon";
 import React from "react";
 
+const btnTypes = {
+    defaultBtn: {
+        typeClasses: "bg-[#0E1422] rounded border py-3 px-6 h-[2.75rem] hover:[#202533]",
+        divClasses: "bg-white",
+        titleClasses: ""
+    },
+    whiteLightBtn: {
+        typeClasses: "bg-white rounded border py-3 px-6 h-[2.75rem]",
+        divClasses: "bg-gray-500",
+        titleClasses: "text-[#5C5F6A] group-hover:text-white "
+    },
+    whiteBoldBtn: {
+        typeClasses: "bg-white rounded border py-3 px-6 h-[2.75rem] border-black hover:bg-[#0E1422]",
+        divClasses: "bg-gray-500",
+        titleClasses: "text-white group-hover:text-black"
+    },
+    whiteBoldSmallBtn: {
+        typeClasses: "bg-white rounded border py-3 px-5 h-[2.75rem] border-black hover:border-[#B6B7BC]",
+        divClasses: "bg-gray-500",
+        titleClasses: "text-white group-hover:text-[#5C5F6A]"
+    },
+    whiteLightRoundedBtn: {
+        typeClasses: "bg-white rounded-3xl w-24 border py-1 px-4 h-8",
+        divClasses: "bg-gray-500",
+        titleClasses: "text-[#5C5F6A]"
+    },
+
+}
+
+type BtnType = keyof typeof btnTypes;
+
 interface ButtonProps {
     icon?: string,
     className?: string,
     title: string,
-    type?: 'button-1' | 'button-2' | 'button-3' | 'button-4' | 'button-5' | 'button-6' | 'button-7' | 'button-8',
+    type?: BtnType,
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void,
     name?: string
 }
 
-export function Button({icon, className, title, type = "button-1", onClick, name}: ButtonProps) {
+export function Button({icon, className, title, type = "defaultBtn", onClick, name}: ButtonProps) {
 
-    let typeClasses = ""
-    let divClasses = ""
-    let titleClasses = ""
-
-    // const btnTypes = {
-    //     primary: 'bg-[#0E1422] rounded border py-3 px-6 h-[2.75rem]',
-    //     secondary: 'bg-[#202533] rounded border py-3 px-6 h-[2.75rem]'
-    // }
+    let typeClasses = btnTypes[type].typeClasses
+    let divClasses = btnTypes[type].divClasses
+    let titleClasses = btnTypes[type].titleClasses
 
 
     // console.log(btnTypes[type])
 
-    switch (type) {
-        case 'button-1': {
-            typeClasses = "bg-[#0E1422] rounded border py-3 px-6 h-[2.75rem]"
-            divClasses = "bg-white"
-            titleClasses = ""
-            break;
-        }
-        case 'button-2': {
-            typeClasses = "bg-[#202533] rounded border py-3 px-6 h-[2.75rem]"
-            divClasses = "bg-white"
-            titleClasses = ""
-            break;
-        }
-        case 'button-3': {
-            typeClasses = "bg-white rounded border py-3 px-6 h-[2.75rem]"
-            divClasses = "bg-gray-500"
-            titleClasses = "text-[#5C5F6A]"
-            break;
-        }
-        case 'button-4': {
-            typeClasses = "bg-white py-3 px-6 h-[2.75rem]"
-            divClasses = "bg-gray-500"
-            titleClasses = "text-[#0E1422]"
-            break;
-        }
-        case 'button-5': {
-            typeClasses = "bg-white rounded border py-3 px-6 h-[2.75rem] border-black"
-            divClasses = "bg-gray-500"
-            titleClasses = "text-[#0E1422]"
-            break;
-        }
-        case 'button-6': {
-            typeClasses = "bg-[#0E1422] rounded border py-3 px-5 h-[2.75rem]"
-            divClasses = "bg-white"
-            titleClasses = ""
-            break;
-        }
-        case 'button-7': {
-            typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem] border-black"
-            divClasses = "bg-gray-500"
-            titleClasses = "text-[#0E1422]"
-            break;
-        }
-        case 'button-8': {
-            typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem]"
-            divClasses = "bg-gray-500"
-            titleClasses = "text-[#5C5F6A]"
-            break;
-        }
-    }
+    // switch (type) {
+    //     case 'button-5': {
+    //         typeClasses = "bg-white rounded border py-3 px-6 h-[2.75rem] border-black"
+    //         divClasses = "bg-gray-500"
+    //         titleClasses = "text-[#0E1422]"
+    //         break;
+    //     }
+    //     case 'button-6': {
+    //         typeClasses = "bg-[#0E1422] rounded border py-3 px-5 h-[2.75rem]"
+    //         divClasses = "bg-white"
+    //         titleClasses = ""
+    //         break;
+    //     }
+    //     case 'button-7': {
+    //         typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem] border-black"
+    //         divClasses = "bg-gray-500"
+    //         titleClasses = "text-[#0E1422]"
+    //         break;
+    //     }
+    //     case 'button-8': {
+    //         typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem]"
+    //         divClasses = "bg-gray-500"
+    //         titleClasses = "text-[#5C5F6A]"
+    //         break;
+    //     }
+    // }
 
     return <button
         name={name}
         onClick={onClick}
-        className={`${typeClasses} overflow-hidden group/button relative transition duration-200 flex items-center ${className}`}>
+        className={`${typeClasses} overflow-hidden group/button relative transition duration-200 flex items-center group justify-center ${className}`}>
         <span className={`${titleClasses} font-medium invert flex items-center`}>{title}</span>
         <div
             className={`${divClasses} group-hover/button:top-[-100%] left-0 absolute w-full h-full top-full transition-all duration-600`}>
         </div>
         {
-            (icon)? <Icon name={`${icon}`}/> : null
+            (icon) ? <Icon name={`${icon}`}/> : null
         }
     </button>;
 }
