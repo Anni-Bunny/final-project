a174af06db1b43c11557c26986cf054b456f02c1
Ani Maghradze <animaghradze50@gmail.com>
2024-10-24 01:28:22 +0400

    Update productListItem.ts

--- src/interfaces/productListItem.ts
+++ src/interfaces/productListItem.ts
@@ -1,10 +1,23 @@
+interface SizeStock {
+    [size: string]: number; // Allows for any size as a key with a number value
+}
+
+interface ColorStock {
+    [color: string]: SizeStock; // Allows for any color as a key with SizeStock as value
+}
+
+interface imageColor {
+    [color: string]: string[];
+}
+
+
 export interface productListItem {
     id: number,
     sku: string,
     name: string,
     price: number,
     description: string,
-    image: string,
+    images: imageColor,
     size: string,
-    stock: number
+    stock: ColorStock
 }
