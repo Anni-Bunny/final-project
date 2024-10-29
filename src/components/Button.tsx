b3f889d2b2cf6a2dc2bb550334afff72de723950
Ani Maghradze <animaghradze50@gmail.com>
2024-10-29 20:36:08 +0400

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,10 +7,10 @@ const btnTypes = {
         divClasses: "bg-white",
         titleClasses: ""
     },
-    whiteLightBtn: {
-        typeClasses: "bg-white rounded border py-3 px-6 h-[2.75rem]",
-        divClasses: "bg-gray-500",
-        titleClasses: "text-[#5C5F6A] group-hover:text-white "
+    LightGrayBtn: {
+        typeClasses: "bg-[#F6F6F6] rounded-3xl border py-3 px-6 h-[2.75rem]",
+        divClasses: "bg-gray-400",
+        titleClasses: "text-[#5C5F6A]"
     },
     whiteBoldBtn: {
         typeClasses: "bg-white rounded border py-3 px-6 h-[2.75rem] border-black hover:bg-[#0E1422]",
@@ -46,36 +46,6 @@ export function Button({icon, className, title, type = "defaultBtn", onClick, na
     let typeClasses = btnTypes[type].typeClasses
     let divClasses = btnTypes[type].divClasses
     let titleClasses = btnTypes[type].titleClasses
-
-
-    // console.log(btnTypes[type])
-
-    // switch (type) {
-    //     case 'button-5': {
-    //         typeClasses = "bg-white rounded border py-3 px-6 h-[2.75rem] border-black"
-    //         divClasses = "bg-gray-500"
-    //         titleClasses = "text-[#0E1422]"
-    //         break;
-    //     }
-    //     case 'button-6': {
-    //         typeClasses = "bg-[#0E1422] rounded border py-3 px-5 h-[2.75rem]"
-    //         divClasses = "bg-white"
-    //         titleClasses = ""
-    //         break;
-    //     }
-    //     case 'button-7': {
-    //         typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem] border-black"
-    //         divClasses = "bg-gray-500"
-    //         titleClasses = "text-[#0E1422]"
-    //         break;
-    //     }
-    //     case 'button-8': {
-    //         typeClasses = "bg-white rounded border py-3 px-5 h-[2.75rem]"
-    //         divClasses = "bg-gray-500"
-    //         titleClasses = "text-[#5C5F6A]"
-    //         break;
-    //     }
-    // }
 
     return <button
         name={name}
