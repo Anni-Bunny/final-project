308127903232a8cb50cf424961c195a0d24d6020
Ani Maghradze <animaghradze50@gmail.com>
2024-10-29 20:36:26 +0400

    Create Stock.tsx

---
+++ src/components/Stock.tsx
@@ -0,0 +1,17 @@
+import {productListItem} from "../interfaces/productListItem";
+
+interface StockParams {
+    product: productListItem,
+    className?: string
+}
+
+export function Stock({product, className}: StockParams) {
+    return (
+        <span
+            className={`border rounded-full py-0.5 px-4 text-sm block w-[6.25rem] ${(product.stock) ? "text-[#0E1422]" : "text-[#E6E7E8]"} ${className}`}>
+            {
+                (product.stock) ? "IN STOCK" : "OUT OF STOCK"
+            }
+        </span>
+    );
+}
