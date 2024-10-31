8f0d6912e0780f867afc125efff2b469027a1c82
Ani Maghradze <animaghradze50@gmail.com>
2024-11-01 00:46:58 +0400

    Update TabSwitcher.tsx

--- src/components/TabSwitcher.tsx
+++ src/components/TabSwitcher.tsx
@@ -2,22 +2,40 @@ import React, {useState} from "react";
 import {Container} from "./Container";
 import {Button} from "./Button";
 
+const types = {
+    vertical: {
+        containerFlexDirection: "flex-col",
+        btnFlexDirection: "flex-row"
+    },
+    horizontal: {
+        containerFlexDirection: "flex-row",
+        btnFlexDirection: "flex-col"
+    },
+}
+type type = keyof typeof types;
+
 interface TabSwitcherProps {
     tabs: { name: string; title: string }[];
     content: { name: string; component: React.ReactNode}[] ;
+    containerClassName?: string;
+    type? : type;
 }
 
-export function TabSwitcher({tabs, content}: TabSwitcherProps) {
+export function TabSwitcher({tabs, content, containerClassName, type = "vertical"}: TabSwitcherProps) {
     const [currentTab, setCurrentTab] = useState<string>(tabs[0].name);
 
     function onChangeTab(event: React.MouseEvent<HTMLButtonElement>) {
         setCurrentTab(event.currentTarget.name);
     }
 
+    let containerFlexDirection = types[type].containerFlexDirection + " " + containerClassName
+    let btnFlexDirection = types[type].btnFlexDirection
+
+
     return (
         <section className="mt-36 mb-44">
-            <Container className="flex flex-col gap-12">
-                <div className="flex items-center mx-auto gap-6 text-sm">
+            <Container className={`flex gap-12 ${containerFlexDirection}`}>
+                <div className={`flex items-center mx-auto gap-6 text-sm ${btnFlexDirection}`}>
                     {tabs.map((tab) => (
                         <Button
                             key={tab.name}
