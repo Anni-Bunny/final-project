76617f6f32d86cab298376ad385da57f350f3852
Ani Maghradze <animaghradze50@gmail.com>
2024-11-04 02:15:09 +0400

    Update ProductReviewList.tsx

--- src/components/ProductReviewList.tsx
+++ src/components/ProductReviewList.tsx
@@ -2,33 +2,61 @@ import {useEffect, useState} from "react";
 import {review} from "../interfaces/review";
 import api from "../classes/API";
 import {ProductReviews} from "./ProductReviews";
+import {Button} from "./Button";
 
 interface ProductReviewListProps {
     productId: number | string
 }
 
+interface reviewRequestResponse {
+    first: number,
+    prev: null|number,
+    next: null|number,
+    last: number,
+    pages: number,
+    items: number,
+    data: review[]
+}
+
+const ReviewRequestResponseDefaultValues ={
+    first: 1,
+    prev: null,
+    next: null,
+    last: 1,
+    pages: 0,
+    items: 0,
+    data: []
+}
+
+
 export function ProductReviewList({productId}: ProductReviewListProps) {
-    const [reviews, setReviews] = useState<review[]>([])
+    const [reviews, setReviews] = useState<reviewRequestResponse>(ReviewRequestResponseDefaultValues)
+    const [averageStars, setAverageStars] = useState<number | null>(null);
 
     useEffect(() => {
         async function getReviews() {
             const reviews = await api.getReviews({productId: productId});
             if (reviews)
-                setReviews(reviews.data);
+                setReviews(reviews);
+
+                const totalStars = reviews.data.reduce((acc: number, review: review) => acc + review.stars, 0);
+                const avgStars = reviews.data.length ? (totalStars / reviews.data.length) : 0;
+                setAverageStars(avgStars);
         }
 
         getReviews();
-    }, []);
+    }, [productId]);
 
     return (
-        <div className="">
-            <div>
-                <h5>Reviews</h5>
-                <p></p>
+        <div className="flex flex-col gap-6 text-[#0E1422]">
+            <div className="border-b">
+                <h5 className="font-semibold text-[1rem] pb-4">Reviews</h5>
+                <p className="pb-10">{averageStars ? averageStars.toFixed(1) : 'No ratings yet'} -{reviews.items} Reviews</p>
+                <Button title={"Write a review"} type={"whiteBoldBtn"} className="mb-10"/>
             </div>
 
             <div>
-                <ProductReviews reviews={reviews}/>
+                <ProductReviews reviews={reviews.data}/>
             </div>
         </div>
     );
