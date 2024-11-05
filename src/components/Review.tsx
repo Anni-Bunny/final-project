c64a1806ae6d9734d54401b6c5595df3db9e985f
Ani Maghradze <animaghradze50@gmail.com>
2024-11-06 02:12:02 +0400

    Update Review.tsx

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -7,6 +7,13 @@ interface ReviewProps{
 }
 
 export function Review({review}:ReviewProps) {
+
+    let stars = []
+
+    for (let i = 0; i < 5; i++) {
+            stars.push(<Icon key={i} name={i < review.stars ? 'star' : 'emptyStar'}/>)
+    }
+
     return (
         <div className="flex gap-6 pt-[2.375rem] pb-[1.688rem]">
             <div className="w-12 h-12 flex items-center justify-center bg-[#F0F1FF] rounded-full px-[0.906rem] py-[0.719rem] text-[#0070F3] font-semibold">{review.name[0]}{review.surName[0]}</div>
@@ -14,12 +21,11 @@ export function Review({review}:ReviewProps) {
                 <div className="flex justify-between">
                     <h3 className="text-sm font-medium mb-1.5">{review.name}{review.surName}</h3>
                     <div className="flex">
-                        {Array(review.stars).map((star,index) => (
-                            <Icon key={index} name="star"/>
-                        ))}
-                        {Array(5-(review.stars)).map((star,index) => (
-                            <Icon key={index + 'empty'} name="emptyStar"/>
-                        ))}
+                        {
+                            stars.map(star => (
+                                star
+                            ))
+                        }
                     </div>
                 </div>
                 <p className="text-xs mb-4 text-[#5C5F6A]">{moment(review.date).fromNow()}</p>
