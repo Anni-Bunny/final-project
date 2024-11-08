b1445d1357eb32e7038b996cbee376d1599fab2f
Ani Maghradze <animaghradze50@gmail.com>
2024-11-08 23:23:41 +0400

    Update cart.ts

--- src/interfaces/cart.ts
+++ src/interfaces/cart.ts
@@ -1,9 +1,12 @@
-interface product{
-    [key:string]: number | string;
+interface cartItem{
+    productId: number,
+    size: string,
+    color: string,
+    quantity: number
 }
 
 export interface cart{
     id: number,
     userId: number,
-    products:product[]
+    products:cartItem[]
 }
