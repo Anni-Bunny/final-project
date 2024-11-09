87638c3531f1d00413fe8f9d5da9e354770629a6
Ani Maghradze <animaghradze50@gmail.com>
2024-11-09 22:08:50 +0400

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,10 +9,10 @@ interface ProductCardProps {
 
 export function ProductCard({product}: ProductCardProps) {
     return (
-        <Card className="space-y-6 px-2 py-4 hover:bg-gray-100 hover:shadow-2xl hover:rounded hover:transition hover:duration-300">
+        <Card className="space-y-6 px-2 py-4 hover:shadow-2xl hover:rounded hover:transition hover:duration-300">
             <img className="bg-[#F6F6F6] h-[25.25rem] bg-cover" src={product.images.default[0]} alt=""/>
             <div className="flex flex-col gap-3">
-                <p>{product.name}</p>
+                <p className="overflow-hidden text-nowrap">{product.name}</p>
                 <div className="flex gap-3.5">
                     <Stock product={product}/>
                     <ProductPrice product={product}/>
