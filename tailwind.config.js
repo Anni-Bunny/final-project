d6ff749bab0c037b10fa8c4429fa0526855ed9ce
Ani Maghradze <animaghradze50@gmail.com>
2024-11-10 03:22:39 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  safelist: [
+    {
+      pattern: /bg-(red|green|blue|yellow|amber|black|gray)-(100|200|300|400|500)/,
+    },
+  ],
   theme: {
     extend: {},
   },
