67a77011bd0602d7e606e5b0786cc05fe49756b9
Ani Maghradze <animaghradze50@gmail.com>
2024-11-12 00:04:29 +0400

    Update ProductDetails.tsx

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -5,7 +5,7 @@ interface ProductDetailsProps {
 export function ProductDetails({description}: ProductDetailsProps) {
     return (
         <>
-            <h5 className="text-[1rem] font-semibold mb-6 text-[#0E1422]">Detail</h5>
+            <h5 className="text-[1rem] font-semibold mb-6 text-[#0E1422]">Details</h5>
             <p className="text-sm text-[#5C5F6A] mb-14">
                 {description}
             </p>
