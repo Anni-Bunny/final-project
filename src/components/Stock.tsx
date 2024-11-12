9cafbb60f141261c62dc7011dd8ccaf493186679
Ani Maghradze <animaghradze50@gmail.com>
2024-11-12 17:52:56 +0400

    Update Stock.tsx

--- src/components/Stock.tsx
+++ src/components/Stock.tsx
@@ -9,11 +9,10 @@ export function Stock({product, className}: StockParams) {
     let sizeFound = false;
 
     Object.keys(product.stock).forEach(color => {
-        
+
         Object.keys(product.stock[color]).forEach(size => {
-            if (product.stock[color][size] > 0) {
+            if (product.stock[color][size] === 0) {
                 sizeFound = true;
-                return
             }
         })
 
@@ -21,7 +20,7 @@ export function Stock({product, className}: StockParams) {
 
     return (
         <span
-            className={`border rounded-full py-0.5 px-4 text-sm block w-[6.25rem] ${(product.stock) ? "text-[#0E1422]" : "text-[#E6E7E8]"} ${className}`}>
+            className={`border rounded-full py-0.5 px-4 text-sm w-[9rem] flex justify-center ${(product.stock) ? "text-[#0E1422]" : "text-[#E6E7E8]"} ${className}`}>
             {
                 (sizeFound) ? "IN STOCK" : "OUT OF STOCK"
             }
