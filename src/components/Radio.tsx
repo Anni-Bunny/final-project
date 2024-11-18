6f21d1cfe5748476d5d42a7a5d7ea0d69b864216
Ani Maghradze <animaghradze50@gmail.com>
2024-11-19 02:40:00 +0400

    Update Radio.tsx

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,3 +1,5 @@
+import {ReactNode} from "react";
+
 const RarioTypes={
     color: {
         parentSpanClasses: "border-transparent p-1 rounded-full",
@@ -12,16 +14,17 @@ const RarioTypes={
 type RadioType = keyof typeof RarioTypes;
 
 interface RadioProps {
-    value?: string,
+    value?: number | string,
     name: string,
     type?: RadioType,
-    label?: string,
+    label?: ReactNode,
     checked?: boolean,
     onChange?:  React.ChangeEventHandler<HTMLInputElement>;
+    classname?: string
 }
 
 
-export function Radio({value, name, type="text", label, checked = false, onChange}: RadioProps) {
+export function Radio({value, name, type="text", label, checked = false, onChange, classname}: RadioProps) {
 
     let parentSpanClasses = RarioTypes[type].parentSpanClasses
     let childSpanClasses = RarioTypes[type].childSpanClasses
@@ -34,7 +37,7 @@ export function Radio({value, name, type="text", label, checked = false, onChang
         <label className="inline-block cursor-pointer">
             <input onChange={onChange} checked={checked} type="radio" name={name} value={value} className="hidden peer"/>
             <span
-                className={`${parentSpanClasses} flex items-center justify-center min-w-8 h-8 border peer-checked:border-[#0E1422] transition-all duration-200`}>
+                className={`${parentSpanClasses} flex items-center justify-center min-w-8 h-8 border peer-checked:border-[#0E1422] transition-all duration-200 ${classname}`}>
                     <span
                         className={`${childSpanClasses} flex items-center justify-center w-full h-full`}>
                         {label}
