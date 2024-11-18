8d7e517dabcce02e4601f13571abc7aa5c071d78
Ani Maghradze <animaghradze50@gmail.com>
2024-11-19 02:42:10 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import {Routes, Route} from 'react-router-dom';
 import {Homepage} from "./pages/Homepage";
 import {Product} from "./pages/Product";
 import {Products} from "./pages/Products";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App(){
     return (
         <div>
+            <ScrollToTop/>
             <Routes>
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="/products/:id" element={<Product/>}/>
                 <Route path="/products" element={<Products/>}/>
-
-
             </Routes>
         </div>
     );
