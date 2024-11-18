be2e0fdfb2b462127f213d3506b806d878ba8160
Ani Maghradze <animaghradze50@gmail.com>
2024-11-19 02:42:27 +0400

    Update Pagination.tsx

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,64 +1,47 @@
-export function Pagination() {
-    return (
-        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-            <div className="flex flex-1 justify-between sm:hidden">
-                <a href="#"
-                   className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-                <a href="#"
-                   className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
-            </div>
-            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                <div>
-                    <p className="text-sm text-gray-700">
-                        Showing
-                        <span className="font-medium">1</span>
-                        to
-                        <span className="font-medium">10</span>
-                        of
-                        <span className="font-medium">97</span>
-                        results
-                    </p>
-                </div>
-                <div>
-                    <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                        <a href="#"
-                           className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                            <span className="sr-only">Previous</span>
-                            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                                 data-slot="icon">
-                                <path fill-rule="evenodd"
-                                      d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                        </a>
-                        <a href="#" aria-current="page"
-                           className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-                        <a href="#"
-                           className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
-                        <a href="#"
-                           className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
-                        <span
-                            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span>
-                        <a href="#"
-                           className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">8</a>
-                        <a href="#"
-                           className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">9</a>
-                        <a href="#"
-                           className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">10</a>
-                        <a href="#"
-                           className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                            <span className="sr-only">Next</span>
-                            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                                 data-slot="icon">
-                                <path fill-rule="evenodd"
-                                      d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                        </a>
-                    </nav>
-                </div>
-            </div>
-        </div>
+import {Icon} from "./Icon";
+import {Radio} from "./Radio";
 
-    );
+interface paginationProps {
+    className?: string,
+    onChange?: React.ChangeEventHandler<HTMLInputElement>,
+    selectedOptionsPage?: number | string,
+    pageCount: number
+}
+
+export function Pagination({className, onChange, selectedOptionsPage, pageCount}: paginationProps) {
+
+    let pages = []
+    for (let i = 1; i <= pageCount; i++) {
+        pages.push(<Radio key={i} name="page" value={i} label={i} checked={selectedOptionsPage == i} onChange={onChange}
+                          classname="border-none peer-checked:bg-gray-100"/>)
+    }
+
+    return (
+        <div
+            className={`flex items-center justify-center border border-[#E9E9EB] max-w-48 w-full py-1 px-2 gap-1 ${className} `}>
+
+            <Radio name={"page"}
+                   onChange={onChange}
+                   checked={selectedOptionsPage === "chevronLeft"}
+                   value={Math.max(Number(selectedOptionsPage) - 1, 1)}
+                   type={"text"}
+                   label={<Icon name="chevronLeft"/>}
+                   classname="border-none peer-checked:bg-gray-100"
+            />
+
+            {
+                pages.map(page => page)
+            }
+
+            <Radio name={"page"}
+                   onChange={onChange}
+                   checked={selectedOptionsPage === "chevronRight"}
+                   value={Math.min(Number(selectedOptionsPage) + 1, pageCount)}
+                   type={"text"}
+                   label={<Icon name="chevronRight"/>}
+                   classname="border-none peer-checked:bg-gray-100"
+            />
+
+        </div>
+    )
 }
