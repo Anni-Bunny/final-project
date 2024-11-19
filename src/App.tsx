1a69f256ec52873d358c1244c52bf27c52e55233
Ani Maghradze <animaghradze50@gmail.com>
2024-11-19 23:37:28 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Homepage} from "./pages/Homepage";
 import {Product} from "./pages/Product";
 import {Products} from "./pages/Products";
 import ScrollToTop from "./components/ScrollToTop";
+import {ShoppingCart} from "./pages/ShoppingCart";
 
 function App(){
     return (
@@ -14,6 +15,7 @@ function App(){
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="/products/:id" element={<Product/>}/>
                 <Route path="/products" element={<Products/>}/>
+                <Route path="/shoppingCart" element={<ShoppingCart/>}/>
             </Routes>
         </div>
     );
