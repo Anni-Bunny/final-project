5cbeaf68367d0cf1d7e112be74a7a40e64561a79
Ani Maghradze <animaghradze50@gmail.com>
2024-11-20 15:34:46 +0400

    Create DropDown.tsx

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import {Button} from './Button';
+
+interface dropdownProps {
+    title?: string;
+    icon?: string,
+    child1?: React.ReactNode,
+    child2?: React.ReactNode,
+    child1ClassName?: string,
+}
+
+export function Dropdown({
+                             title,
+                             icon,
+                             child1,
+                             child2,
+                             child1ClassName,
+                         }: dropdownProps) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsVisible((prev) => !prev);
+    };
+
+    return (
+        <div className="flex relative w-full justify-end"
+             onMouseLeave={() => setIsVisible(false)}>
+            <div className="relative">
+                <Button
+                    title={title}
+                    icon={icon}
+                    className={"relative"}
+                    type={"whiteSmallBtn"}
+                    onClick={toggleDropdown}
+                />
+                <span className={`absolute ${child1ClassName}`}>
+                    {child1}
+                </span>
+            </div>
+
+            <div
+                className={`absolute right-2 bottom-0 translate-y-full z-10 ${isVisible ? '' : 'hidden'} bg-gray-500 rounded shadow-2xl w-44`}>
+                {child2}
+            </div>
+        </div>
+    );
+}
