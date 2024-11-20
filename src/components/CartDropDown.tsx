8219f3b7f00ed3087f585558d9cbfe826b36e932
Ani Maghradze <animaghradze50@gmail.com>
2024-11-20 21:01:03 +0400

    Update CartDropDown.tsx

--- src/components/CartDropDown.tsx
+++ src/components/CartDropDown.tsx
@@ -1,6 +1,7 @@
 import {Dropdown} from "./DropDown";
 import {useSelector} from "react-redux";
 import {RootState} from "../store/store";
+import {CartItem} from "./CartItem";
 
 export function CartDropDown() {
     const cart = useSelector((state: RootState) => state.cart)
@@ -16,9 +17,15 @@ export function CartDropDown() {
                   child1={totalQuantity}
                   child1ClassName="top-0 right-0 bg-red-100 w-5 h-5 rounded-full flex items-center justify-center "
                   child2={
-<>
-</>
+                      <>
+                          {
+                              cart.products.map((product) =>
+                              <CartItem product={product}/>
+                              )
+                          }
+                      </>
                   }
+                  child2ClassName="max-w-96 w-full"
         />
     );
 }
