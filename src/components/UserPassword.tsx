5a880564342e4b44d5c1449619797fedc260a154
Ani Maghradze <animaghradze50@gmail.com>
2024-11-24 02:29:09 +0400

    Update UserPassword.tsx

--- src/components/UserPassword.tsx
+++ src/components/UserPassword.tsx
@@ -7,15 +7,16 @@ import React from "react";
 export function UserPassword() {
     return (
         <Container>
-            <div className="my-32 flex flex-col items-center justify-center max-w-96 w-full mx-auto">
-                <h5 className="text-[#0E1422] font-semibold text-lg py-4 mb-16">Change Password</h5>
-                <Input inputType={"password"} className="w-full mb-6" inputClassName={"w-full"} label={"New password"}
-                       placeholder={"Enter Your Password"}/>
-                <Input inputType={"password"} className="w-full mb-6" inputClassName={"w-full"} label={"Confirm Password"}
-                       placeholder={"Confirm Your Password"}/>
+            <div className="flex flex-col max-w-96 w-full">
+                <h5 className="text-[#0E1422] font-semibold text-lg flex items-start mb-12">Change Password</h5>
+                <div className="flex flex-col gap-4 mb-16">
+                    <Input inputType={"password"} className="w-full" inputClassName={"w-full"} label={"New password"}
+                           placeholder={"Enter Your Password"}/>
+                    <Input inputType={"password"} className="w-full" inputClassName={"w-full"} label={"Confirm Password"}
+                           placeholder={"Confirm Your Password"}/>
+                </div>
 
-
-                <Link to={"/resetPassword"} className="w-full"><Button title="Change password" className="w-full mb-8"/></Link>
+                <Link to={""} className="w-full"><Button title="Change password" className=" mb-8"/></Link>
 
             </div>
         </Container>
