8e73a5aeb5f630218bbd5ce015aab0b061c186a4
Ani Maghradze <animaghradze50@gmail.com>
2024-11-24 02:26:47 +0400

    Update order.ts

--- src/interfaces/order.ts
+++ src/interfaces/order.ts
@@ -1,10 +1,20 @@
-import {cartItem} from "./cart";
 import {address} from "./user"
+
+export interface orderItem {
+    image: string,
+    name: string,
+    price: number,
+    productId: number,
+    size: string,
+    color: string,
+    quantity: number,
+    sku: string
+}
 
 export interface order {
     id: string | number,
     status: string,
     createdAt: string,
-    products: cartItem[],
+    products: orderItem[],
     address: address
 }
