61fb71aea03bf3168d0a388457e842672cc204fa
Ani Maghradze <animaghradze50@gmail.com>
2024-11-24 02:28:26 +0400

    Update UserOrders.tsx

--- src/components/UserOrders.tsx
+++ src/components/UserOrders.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import api from "../classes/API";
 import {order} from "../interfaces/order";
-import {OrderedItem} from "./OrderedItem";
+import {Order} from "./Order";
 import {Link} from "react-router-dom";
 import {Button} from "./Button";
 
@@ -23,7 +23,7 @@ export function UserOrders() {
             {
                 orders && orders.length > 0 ? (
                     orders.map((order, index) => (
-                        <OrderedItem key={index} order={order} />
+                        <Order key={index} order={order} />
                     ))
                 ) : (
                     <div className="flex flex-col items-center justify-center gap-8">
