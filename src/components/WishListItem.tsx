24ec7ba95e40fe4b140f0715c258970fbf7f3ee0
Ani Maghradze <animaghradze50@gmail.com>
2024-11-24 03:43:45 +0400

    Update WishListItem.tsx

--- src/components/WishListItem.tsx
+++ src/components/WishListItem.tsx
@@ -1,32 +1,34 @@
-import {order} from "../interfaces/order";
 import React from "react";
 import {Button} from "./Button";
+import {wishListItem} from "../interfaces/wishList";
+import {Link} from "react-router-dom";
 
-interface orderedItemProps {
-    order: order["userId"],
+interface WishListItemProps {
+    product: wishListItem,
 }
 
-export function WishListItem({order}: orderedItemProps) {
+export function WishListItem({product}: WishListItemProps) {
     return (
-        <div className="flex items-center justify-between">
-            <div className="flex items-center">
-                <img className="bg-cover w-28" src={order.image} alt=""/>
-                <div className="flex flex-col gap-4">
-                    <p className="max-w-44 overflow-hidden font-semibold">{order.name}</p>
-                    <div className="flex items-center gap-1 justify-center">
-                        <p>Ordered on: - </p>
-                        <span className="font-semibold"> {order.orderedOn} </span>
-                    </div>
-                    <Button title={"Remove item"} type={"whiteSmallBtn"}/>
+        <div className="flex items-center justify-between py-8 border-b last:border-none">
+            <div className="flex items-center gap-8">
 
+                <Link to={`/products/${product.productId}`}>
+                    <img className="max-w-24" src={product.image}
+                         alt=""/> </Link>
+
+                <div className="flex flex-col gap-4 items-start">
+                <h3 className="font-medium">{product.name}</h3>
+                    <div className="flex items-center gap-1 justify-center text-[#717171] text-sm">
+                        <p>Added on: </p>
+                        <span className="font-semibold"> {product.addedAt} </span>
+                    </div>
                 </div>
             </div>
 
             <div className="flex justify-between items-center gap-10">
-                <div className="flex">
-                    <span className="font-semibold"> {`$${order.price}.00`} </span>
-                </div>
-                <Button title={"View item"} type={"whiteBtn"}/>
+                <span className="font-semibold"> {`$${product.price}.00`} </span>
+                <Button title={"Remove item"} type={"whiteSmallBtn"} className="flex text-start"/>
+                <Button title={"Add to cart"} type={"whiteBtn"}/>
             </div>
         </div>
     );
