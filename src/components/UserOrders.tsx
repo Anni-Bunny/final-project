e42a336e8ce906a3c11e1bcafa1556b2fd060bba
Ani Maghradze <animaghradze50@gmail.com>
2024-11-23 22:58:41 +0400

    Update UserOrders.tsx

--- src/components/UserOrders.tsx
+++ src/components/UserOrders.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import api from "../classes/API";
 import {order} from "../interfaces/order";
 import {OrderedItem} from "./OrderedItem";
+import {Link} from "react-router-dom";
+import {Button} from "./Button";
 
 export function UserOrders() {
     const [orders, setOrders] = useState<order[]>();
@@ -19,10 +21,17 @@ export function UserOrders() {
     return (
         <div className="flex flex-col gap-4">
             {
-                orders &&
-                orders.map((order, index) => (
-                    <OrderedItem key={index} order={order}/>
-                ))
+                orders && orders.length > 0 ? (
+                    orders.map((order, index) => (
+                        <OrderedItem key={index} order={order} />
+                    ))
+                ) : (
+                    <div className="flex flex-col items-center justify-center gap-8">
+                        <img src="/images/emptyOrders.png" alt="Description"/>
+                        <p className="text-sm text-[#5C5F6A]">Your order history is waiting to be filled.</p>
+                        <Link to="/products"><Button title="Start Shopping" icon="arrowRight"/></Link>
+                    </div>
+                )
             }
         </div>
     );
