03ed3cb7f3146927f2ca0635c43efbd941c2b005
Ani Maghradze <animaghradze50@gmail.com>
2024-11-23 22:58:13 +0400

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,7 +10,7 @@ export function Input({label, className, inputType, placeholder, inputClassName}
     return (
         <div className={`flex flex-col ${className}`}>
             {
-                (label) ? <label className="text-sm/6 font-medium text-black">{label}</label> : null
+                (label) ? <label className="text-sm/6 font-medium text-[#474B57]">{label}</label> : null
             }
             <input className={`${inputClassName} rounded-md border border-[#E6E7E8] py-2.5 px-[0.938rem] h-[2.813rem] w-80 text-black text-sm
             focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 required:`}
