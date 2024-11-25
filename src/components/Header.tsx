04056ed8b05d8b8953a4d58c20b6facc5b2239a0
Ani Maghradze <animaghradze50@gmail.com>
2024-11-26 01:39:11 +0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,16 @@ import {Link} from "react-router-dom";
 import {Icon} from "./Icon";
 import {Container} from "./Container";
 import {CartDropDown} from "./CartDropDown";
+import {useSelector} from "react-redux";
+import {RootState} from "../store/store";
+import {CartItem} from "./CartItem";
+import React from "react";
+import {Button} from "./Button";
+import {Dropdown} from "./DropDown";
+import {Logout} from "./Logout";
 
 export function Header() {
+    const user = useSelector((state: RootState) => state.user.data)
     return (
         <Container className="h-20 border-b border-[#F6F6F6] bg-white flex justify-center items-center">
             <header className="h-11 flex justify-between w-full">
@@ -19,16 +27,39 @@ export function Header() {
                         <Link to={"/contact"}>Contact</Link>
                     </div>
                 </div>
-                <div className="flex gap-8 h-11 items-center justify-between">
+                <div className="flex gap-4 h-11 items-center justify-between">
                     <div className="flex border border-1 rounded-md gap-2 px-4 py-2.5 h-11 items-center w-72">
                         <Icon name="search"/>
                         <input type="search" className="h-6 outline-none"/>
                     </div>
                     <CartDropDown/>
-                    <Link to={"/profile"}><Icon name="user-1"/></Link>
+
+                    {
+                        user ?
+                            (
+                                <div>
+                                    <Dropdown
+                                        icon="user-1"
+                                        child2={
+                                        <div className="py-2 w-28">
+                                            <Link to={"/profile"}><Button title="Profile" type={"whiteSmallBtn"}/></Link>
+                                            <Logout/>
+                                        </div>
+                                        }
+                                    />
+                                </div>
+                            )
+                            :
+                            (
+                                <div className="flex h-11 items-center justify-between">
+                                    <Link to={"/login"}><Button title="Login" type={"whiteSmallBtn"}/></Link>
+                                    <Link to={"/signup"}><Button title="Signup" type={"whiteSmallBtn"}/></Link>
+                                </div>
+                            )
+                    }
                 </div>
             </header>
         </Container>
-)
-    ;
+    )
+        ;
 }
