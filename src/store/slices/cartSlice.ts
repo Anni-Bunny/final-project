2291517e8c387e04fbd2c06788f7e793ad0ab9fb
Ani Maghradze <animaghradze50@gmail.com>
2024-11-27 01:11:28 +0400

    Update cartSlice.ts
    updated api on every cart change

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,6 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit'
 import type {PayloadAction} from '@reduxjs/toolkit'
 import {cart, cartItem} from "../../interfaces/cart";
+import api from "../../classes/API";
 
 
 const initialState: cart = {
@@ -13,12 +14,16 @@ export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        loadUserCart: (state,action:PayloadAction<cart>) => action.payload,
+        loadUserCart: (state, action: PayloadAction<cart>) => action.payload,
         addProduct: (state, action: PayloadAction<cartItem>) => {
             state.products.push(action.payload)
+            api.updateUserCart(state)
+
         },
         removeProduct: (state, action: PayloadAction<number>) => {
             state.products = state.products.filter(product => product.productId !== action.payload)
+            api.updateUserCart(state)
+
         },
         incrementProduct: (state, action: PayloadAction<string>) => {
             state.products = state.products.map(product => {
@@ -28,6 +33,8 @@ export const cartSlice = createSlice({
 
                 return product
             })
+            api.updateUserCart(state)
+
         },
         decrementProduct: (state, action: PayloadAction<string>) => {
             state.products = state.products.map(product => {
@@ -38,12 +45,20 @@ export const cartSlice = createSlice({
                 return product
 
             }).filter(product => product.quantity > 0)
+            api.updateUserCart(state)
         },
         clearCart: () => initialState
     },
 })
 
 // Action creators are generated for each case reducer function
-export const {loadUserCart,addProduct, removeProduct, incrementProduct, decrementProduct, clearCart} = cartSlice.actions
+export const {
+    loadUserCart,
+    addProduct,
+    removeProduct,
+    incrementProduct,
+    decrementProduct,
+    clearCart
+} = cartSlice.actions
 
 export default cartSlice.reducer
