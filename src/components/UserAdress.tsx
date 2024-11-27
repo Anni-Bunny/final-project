5967f51a3665fd8fefe8cea5ded77191866a92a4
Ani Maghradze <animaghradze50@gmail.com>
2024-11-28 03:43:48 +0400

    Update UserAdress.tsx

--- src/components/UserAdress.tsx
+++ src/components/UserAdress.tsx
@@ -2,8 +2,12 @@ import {Container} from "./Container";
 import {Input} from "./Input";
 import React from "react";
 import {Button} from "./Button";
+import {useSelector} from "react-redux";
+import {RootState} from "../store/store";
 
 export function UserAdress() {
+    const user = useSelector((state: RootState) => state.user.data)
+
     return (
         <Container>
             <div className="flex flex-col w-full">
@@ -11,18 +15,13 @@ export function UserAdress() {
                     <h5 className="text-[#0E1422] font-semibold text-lg py-4 mb-8">Shipping Address</h5>
 
                     <div className="flex flex-col gap-4 mb-16">
-                        <Input inputType={"text"} label={"Street Address"} inputClassName={"w-full"}
-                               placeholder={"Enter Your Street Address"}/>
+                        <Input value={user?.address?.street} inputType={"text"} label={"Street Address"} inputClassName={"w-full"}/>
 
                         <div className="grid grid-cols-2 gap-4  ">
-                            <Input inputType={"text"} inputClassName={"w-full"} label={"City"}
-                                   placeholder={"Enter Your City"}/>
-                            <Input inputType={"text"} inputClassName={"w-full"} label={"State"}
-                                   placeholder={"Enter Your State"}/>
-                            <Input inputType={"text"} inputClassName={"w-full"} label={"Zip Code"}
-                                   placeholder={"Enter Zip Code"}/>
-                            <Input inputType={"text"} inputClassName={"w-full"} label={"Country"}
-                                   placeholder={"Enter Your Country"}/>
+                            <Input value={user?.address?.city} inputType={"text"} inputClassName={"w-full"} label={"City"}/>
+                            <Input value={user?.address?.state} inputType={"text"} inputClassName={"w-full"} label={"State"}/>
+                            <Input value={user?.address?.zipcode} inputType={"text"} inputClassName={"w-full"} label={"Zip Code"}/>
+                            <Input value={user?.address?.country} inputType={"text"} inputClassName={"w-full"} label={"Country"}/>
                         </div>
                     </div>
 
