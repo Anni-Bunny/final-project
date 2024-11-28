a69f587e3e4c64fcfde3a0f680654653f4c13442
Ani Maghradze <animaghradze50@gmail.com>
2024-11-28 18:19:38 +0400

    Update WishListItem.tsx

--- src/components/WishListItem.tsx
+++ src/components/WishListItem.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import {Button} from "./Button";
 import {wishListItem} from "../interfaces/wishList";
 import {Link} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {removeProduct} from "../store/slices/wishListSlice";
 
 interface WishListItemProps {
     product: wishListItem,
 }
 
 export function WishListItem({product}: WishListItemProps) {
+    const dispatch = useDispatch();
     return (
         <div className="flex items-center justify-between py-8 border-b last:border-none">
             <div className="flex items-center gap-8">
@@ -27,8 +30,8 @@ export function WishListItem({product}: WishListItemProps) {
 
             <div className="flex justify-between items-center gap-10">
                 <span className="font-semibold"> {`$${product.price}.00`} </span>
-                <Button title={"Remove item"} type={"whiteSmallBtn"} className="flex text-start"/>
-                <Button title={"Add to cart"} type={"whiteBtn"}/>
+                <Button onClick={() => dispatch(removeProduct(product.productId))} title={"Remove item"} type={"whiteSmallBtn"} className="flex text-start"/>
+                <Link to={`/products/:${product.productId}`}><Button title={"Add to cart"} type={"whiteBtn"}/></Link>
             </div>
         </div>
     );
