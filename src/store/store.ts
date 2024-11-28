8a4159794ec12a03e297b81a7c76bb21c9121fa2
Ani Maghradze <animaghradze50@gmail.com>
2024-11-28 23:55:57 +0400

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,12 +5,13 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // Uses localStorage by default
 import { combineReducers } from 'redux';
 import {wishListSlice} from "./slices/wishListSlice";
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
 
 // Set up the persist configuration
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['cart', 'user'], // Persist cart and user slices
+    whitelist: ['cart', 'user','wishList'], // Persist cart and user slices
 };
 
 // Combine reducers
@@ -26,6 +27,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 // Configure the store with the persisted reducer
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        })
 });
 
 // Create persistor for the app
