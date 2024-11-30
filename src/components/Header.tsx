26774fef0af2a1b8ae20bba17cb6ad91297ad551
Ani Maghradze <animaghradze50@gmail.com>
2024-12-01 02:51:42 +0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,6 +45,7 @@ export function Header() {
                                             <Logout/>
                                         </div>
                                         }
+                                        child2ClassName="right-2"
                                     />
                                 </div>
                             )
