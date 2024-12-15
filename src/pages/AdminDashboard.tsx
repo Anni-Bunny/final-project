b2506890b33e1e1fd6f3cc54975d7925f82552b7
Ani Maghradze <animaghradze50@gmail.com>
2024-12-15 04:51:26 +0400

    added charts

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -9,9 +9,12 @@ import {renponse} from "../interfaces/response";
 import {order} from "../interfaces/order";
 import {useNavigate} from "react-router-dom";
 import {product} from "../interfaces/product";
-import { Doughnut } from 'react-chartjs-2';
-import {Chart, ArcElement} from 'chart.js'
-Chart.register(ArcElement);
+import {Doughnut, Line} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
+import {Chart, registerables} from 'chart.js'
+
+
+Chart.register(...registerables);
 
 interface Data {
     image: ReactNode,
@@ -39,14 +42,15 @@ interface selectedOptions {
 
 export function AdminDashboard() {
     const [data, setData] = useState<Data[]>([])
-    const [response, setResponse] = useState<renponse>(defaultResponse)
+    const [ordersResponse, setOrdersResponse] = useState<renponse>(defaultResponse)
+    const [usersResponse, setUsersResponse] = useState<renponse>(defaultResponse);
     const [BestSellers, setBestSellers] = useState<product[]>([])
     const navigate = useNavigate()
 
     useEffect(() => {
         async function getOrders() {
             const response = await api.getOrders({_page: 1, _per_page: 4});
-            setResponse(response);
+            setOrdersResponse(response);
 
             let updatedData: Data[] = []
 
@@ -78,17 +82,23 @@ export function AdminDashboard() {
         }
 
         async function getBestSellers() {
-            const bestSellers = await api.getProducts({_sort: "-score", _per_page: 3, _page:1});
+            const bestSellers = await api.getProducts({_sort: "-score", _per_page: 3, _page: 1});
             if (bestSellers)
                 setBestSellers(bestSellers.data);
         }
 
+        async function getUsers() {
+            const response = await api.getUsers({_page: 1});
+            if (response)
+                setUsersResponse(response);
+        }
+
         getOrders();
         getBestSellers();
+        getUsers()
 
         topFunction()
     }, []);
-
 
     let links = [
         {
@@ -112,18 +122,48 @@ export function AdminDashboard() {
         navigate('/admin/Orders')
     }
 
-    const [chartDataSet, setChartDataSet] = useState(            {
+    const [doughnutDataSet, setDoughnutDataSet] = useState({
         data: [300, 50, 100],
         backgroundColor: ['#4078FF', '#A8B2FF', '#638fcd'],
         hoverBackgroundColor: ['#FF6347', '#ff835d', '#FFD700'],
     });
 
-    const chartData = {
-        labels: ['Red', 'Blue', 'Yellow'],
+    const DoughnutData = {
         datasets: [
-            chartDataSet
+            doughnutDataSet
         ],
     };
+
+
+    const barData = {
+        labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '',],
+        datasets: [{
+            label: '',
+            data: [65, 59, 80, 81, 56, 55, 40, 10, 15, 20, 40, 63, 14, 70],
+            backgroundColor:
+                '#4078FF',
+
+            borderColor:
+                'rgb(75, 192, 192)',
+
+            borderWidth: 1
+        }]
+    };
+
+
+    const lineData = {
+        labels: ['', '', '', '', '', '', '',],
+        datasets: [{
+            label: '',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#4078FF',
+            tension: 0.1
+        }]
+    };
+
+    const divWidth = ((ordersResponse.items ?? 1)/1000) * 100
+
 
     return (
         <div className='flex flex-col gap-10 h-full'>
@@ -134,11 +174,62 @@ export function AdminDashboard() {
 
             <div className="flex flex-col max-h-[48rem] h-full gap-8">
                 <div className="flex gap-10 h-1/3">
-                    <div className="bg-white border border-[#E9E9EB] rounded-lg h-full w-1/3"></div>
+                    <div className="bg-white border border-[#E9E9EB] rounded-lg h-full w-1/3 flex flex-col p-8 gap-4 ">
+                        <div className="flex justify-between">
+                            <div className="flex flex-col gap-1.5">
+                                <h3 className="font-semibold text-lg">Total Sales</h3>
+                                <p className="text-sm text-[#5C5F6A]">THIS MONTH</p>
+                            </div>
+                            <h2 className="font-bold text-2xl">$4,235</h2>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <Bar data={barData} options={{
+                                plugins: {legend: {display: false}},
+                                responsive: true,
+                                maintainAspectRatio: false
+                            }}/>
+                        </div>
+                    </div>
 
                     <div className="w-2/3 flex gap-10">
-                        <div className="bg-white border border-[#E9E9EB] rounded-lg h-full w-full"></div>
-                        <div className="bg-white border border-[#E9E9EB] rounded-lg h-full w-full"></div>
+                        <div
+                            className="bg-white border border-[#E9E9EB] rounded-lg h-full w-full flex p-8 justify-between flex-col gap-4">
+                            <div className="flex justify-between">
+                                <div className="flex flex-col gap-1.5">
+                                    <h3 className="font-semibold text-lg">Customers</h3>
+                                    <p className="text-sm text-[#5C5F6A]">THIS MONTH</p>
+                                </div>
+                                <h2 className="font-bold text-2xl">{usersResponse.items}</h2>
+                            </div>
+
+                            <div className="flex justify-center">
+                                <Line data={lineData} options={{
+                                    plugins: {legend: {display: false}},
+                                    responsive: true,
+                                    maintainAspectRatio: false
+                                }}/>
+                            </div>
+                        </div>
+
+                        <div
+                            className="bg-white border border-[#E9E9EB] rounded-lg h-full w-full flex p-8 justify-between flex-col gap-4">
+                            <div className="flex justify-between">
+                                <div className="flex flex-col gap-1.5">
+                                    <h3 className="font-semibold text-lg">Orders</h3>
+                                    <p className="text-sm text-[#5C5F6A]">Monthly GOALS : 1,000</p>
+                                </div>
+                                <h2 className="font-bold text-2xl">{ordersResponse.items}</h2>
+                            </div>
+
+                            <div className="flex flex-col gap-4">
+                                <p>{1000 - (ordersResponse.items ?? 0) } Left</p>
+
+                                <div className="relative max-w-[24.5rem] w-full h-3 rounded-lg bg-[#F0F1FF] overflow-hidden">
+                                    <div style={ {width: (ordersResponse.items ?? 0)/10 +"%"} } className=" h-full max-w-[24.5rem] bg-[#4078FF]"></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -159,13 +250,15 @@ export function AdminDashboard() {
                             <div className="flex flex-col gap-3">
                                 {
                                     BestSellers.map((product, index) =>
-                                        <span key={index} className="rounded-2xl border py-1 px-4 text-[#5C5F6A] max-w-max">{product.name} - <span className="text-[#0E1422] font-semibold">${chartDataSet.data[index]}</span></span>
+                                        <span key={index}
+                                              className="rounded-2xl border py-1 px-4 text-[#5C5F6A] max-w-max">{product.name} - <span
+                                            className="text-[#0E1422] font-semibold">${doughnutDataSet.data[index]}</span></span>
                                     )
                                 }
                             </div>
 
-                            <div className="max-w-24">
-                                <Doughnut data={chartData} />
+                            <div className="max-w-28">
+                                <Doughnut data={DoughnutData}/>
                             </div>
 
                         </div>
