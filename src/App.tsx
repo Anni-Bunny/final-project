3c4a183ef57aaa469334287026a33ca4e4665e1b
Ani Maghradze <animaghradze50@gmail.com>
2024-12-20 00:16:52 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route, useLocation} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import {Homepage} from "./pages/Homepage";
 import {Product} from "./pages/Product";
 import {Products} from "./pages/Products";
