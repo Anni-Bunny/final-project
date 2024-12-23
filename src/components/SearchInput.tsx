9ea779fc4c672b66e7d3d5f2b71ec9f5c94da02d
Ani Maghradze <animaghradze50@gmail.com>
2024-12-23 20:47:31 +0400

    Update SearchInput.tsx

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,47 +1,41 @@
 import React, {useState} from 'react';
-import {Icon} from "./Icon";
+import {Button} from "./Button";
+import {useNavigate} from "react-router-dom";
 
 interface searchInputProps {
     placeholder?: string;
-    onSearch: (query: string) => void;
-    debounceTime?: number; // Optional debounce time in ms (default: 500ms)
 }
 
-export function SearchInput({
-                                placeholder = 'Search...',
-                                onSearch,
-                                debounceTime = 500,
-                            }: searchInputProps) {
-
+export function SearchInput({placeholder = "Search..."}: searchInputProps) {
     const [query, setQuery] = useState<string>('');
-    const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+    const navigate = useNavigate();
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newQuery = e.target.value;
-        setQuery(newQuery);
+        setQuery(e.currentTarget.value);
+    };
 
-        // Debounce search input
-        if (timer) {
-            clearTimeout(timer);
+    const handleSearch = () => {
+        navigate(`/products?search=${query}`);
+    };
+
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            handleSearch();
+            setQuery('')
         }
-
-        const newTimer = setTimeout(() => {
-            onSearch(newQuery);
-        }, debounceTime);
-
-        setTimer(newTimer);
     };
 
 
     return (
         <div className="relative flex border border-1 rounded-md gap-2 px-4 py-2.5 h-11 items-center w-72">
-            <Icon name="search"/>
+            <Button icon="search" type="CleanBtn" onClick={handleSearch}/>
             <input
                 type="text"
                 value={query}
                 onChange={handleChange}
                 placeholder={placeholder}
                 className="h-6 outline-none"
+                onKeyDown={handleKeyPress}
             />
 
         </div>
